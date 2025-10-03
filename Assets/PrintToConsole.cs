f80a1ff2b076e44bed9c7bea9db87bc64f4303ec
12shamali12 <omar.sh.880.oa@gmail.com>
2025-10-03 06:25:36 +0300

    Initial commit without temp files

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("I’m not scary… I’m just misunderstood");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
